5ca3c8264387ed699256521ebe10608a25f98cfa
habborge <borgeh@uninorte.edu.co>
2019-01-23 15:48:08 -0500

    adicionando middleware de mensajes de error

--- server/index.js
+++ server/index.js
@@ -2,8 +2,30 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello hab');
+app.get('/', (req, res, next) => {
+  res.json({
+    message: 'Hello hab let´s do it',
+  });
+});
+
+// No route found handler
+app.use((req, res, next) => {
+  res.status(404);
+  res.json({
+    message: 'Error. Route not found',
+  });
+});
+
+app.use((err, req, res, next) => {
+  const {
+    statusCode = 500,
+      message,
+  } = err;
+
+  res.status(statusCode);
+  res.json({
+    message,
+  });
 });
 
 module.exports = app;
